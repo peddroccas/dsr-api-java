a76e752388ce25fefbdc54cc057c0cefedf0fa26
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-12 12:11:57 -0300

    feat: initial commit

---
+++ src/test/java/br/com/dsr/DsrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
