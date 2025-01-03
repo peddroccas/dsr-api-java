f691f0e93bf40ffb1fb48af49d078b67cf684e32
Pedro Antunes <pedroabernardes11@gmail.com>
2025-01-03 11:15:03 -0300

    feat: update password

--- src/main/java/br/com/dsr/modules/users/controllers/UserController.java
+++ src/main/java/br/com/dsr/modules/users/controllers/UserController.java
@@ -8,10 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +20,7 @@ import br.com.dsr.modules.users.DTOs.UserRecordDTO;
 import br.com.dsr.modules.users.useCases.AuthenticateUseCase;
 import br.com.dsr.modules.users.useCases.DeleteUserUseCase;
 import br.com.dsr.modules.users.useCases.ProfileUseCase;
+import br.com.dsr.modules.users.useCases.UpdatePasswordUseCase;
 import br.com.dsr.modules.users.useCases.UpdateUserUseCase;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -35,6 +36,8 @@ public class UserController {
     private UpdateUserUseCase updateUserUseCase;
     @Autowired
     private DeleteUserUseCase deleteUserUseCase;
+    @Autowired
+    private UpdatePasswordUseCase updatePasswordUseCase;
 
     @GetMapping("/profile")
     public ResponseEntity<Object> profile(HttpServletRequest request) {
@@ -79,6 +82,18 @@ public class UserController {
     public ResponseEntity<Object> delete(@PathVariable UUID id) {
         try {
             deleteUserUseCase.execute(id);
+            return ResponseEntity.ok().body(null);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PatchMapping("/users")
+    public ResponseEntity<Object> updatePassword(HttpServletRequest request, @RequestBody String password) {
+        var id = UUID.fromString(request.getAttribute("id").toString());
+
+        try {
+            updatePasswordUseCase.execute(id, password);
             return ResponseEntity.ok().body(null);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
---
+++ src/main/java/br/com/dsr/modules/users/useCases/UpdatePasswordUseCase.java
@@ -0,0 +1,32 @@
+package br.com.dsr.modules.users.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.com.dsr.modules.users.entities.UserEntity;
+import br.com.dsr.modules.users.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UpdatePasswordUseCase {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserEntity execute(UUID id, String password) {
+        var user = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    throw new EntityNotFoundException();
+                });
+
+        var passwordEncoded = passwordEncoder.encode(password).toString();
+        user.setPassword(passwordEncoded);
+
+        return userRepository.save(user);
+    }
+}
