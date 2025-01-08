9f15184bd0db87c0cc0caa972b5b75dc1454ee0a
Pedro Antunes <pedroabernardes11@gmail.com>
2025-01-08 09:18:08 -0300

    feat: find stores

--- src/main/java/br/com/dsr/modules/stores/controllers/StoreController.java
+++ src/main/java/br/com/dsr/modules/stores/controllers/StoreController.java
@@ -3,8 +3,12 @@ package br.com.dsr.modules.stores.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dsr.modules.stores.DTOs.StoreRecordDTO;
+import br.com.dsr.modules.stores.entities.StoreEntity;
 import br.com.dsr.modules.stores.useCases.CreateStoreUseCase;
+import br.com.dsr.modules.stores.useCases.FindStoresUseCase;
 import jakarta.validation.Valid;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +17,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/stores")
@@ -20,6 +26,8 @@ public class StoreController {
 
     @Autowired
     private CreateStoreUseCase createStoreUseCase;
+    @Autowired
+    private FindStoresUseCase findStoresUseCase;
 
     @PostMapping("")
     @PreAuthorize("hasRole('ADMIN')")
@@ -31,6 +39,19 @@ public class StoreController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Object> findAll() {
+        try {
+            var stores = this.findStoresUseCase.execute();
+
+            return ResponseEntity.ok().body(stores);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 
 }
--- src/main/java/br/com/dsr/modules/stores/useCases/FindStoresUseCase.java
+++ src/main/java/br/com/dsr/modules/stores/useCases/FindStoresUseCase.java
@@ -1,8 +1,11 @@
 package br.com.dsr.modules.stores.useCases;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.dsr.modules.stores.entities.StoreEntity;
 import br.com.dsr.modules.stores.repositories.StoreRepository;
 
 @Service
@@ -10,7 +13,9 @@ public class FindStoresUseCase {
     @Autowired
     private StoreRepository storeRepository;
 
-    public void execute() {
+    public List<StoreEntity> execute() {
         var stores = this.storeRepository.findAll();
+
+        return stores;
     }
 }
